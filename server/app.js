5b04a2bfddff654b88d79ac34775df08e6d076f5
Railson Carvalho <railson031@hotmail.com>
2022-12-26 20:44:06 -0300

    :package: first part of the user deployment: routes, utils, models, controllers.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import session from 'express-session';
+
+import userRouter from './routes/user.js';
+import { connectPassport } from './utils/Provider.js';
+import passport from 'passport';
 
 const app = express();
 
 dotenv.config({ path: "./config/config.env" });
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+})
+);
+
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+connectPassport();
+
+app.use("/api/v1", userRouter);
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import connectDB from './config/database.js';
 
 connectDB();
 
-app.get('/', (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.send("<h1>Working</h1>")
 })
 
